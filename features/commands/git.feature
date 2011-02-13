1ef36c6726d6dc02d4aabef878a63b2a114b0746
Bram Swenson <bram@craniumisajar.com>
2011-02-12 21:34:23 -0600

    [GIT] refactored git to handle some commands with more opinions

--- features/commands/git.feature
+++ features/commands/git.feature
@@ -6,6 +6,12 @@ Feature: Manage vimpack git repo
 
   Scenario: Initialize git remote
     Given an initialized vimpack in "test_vimpack"
-    When I run "vimpack git remote add origin git@github.com:bramswenson/vimpack-repo.git"
+    When I run "vimpack git remote git@github.com:bramswenson/vimpack-repo.git"
     Then the exit status should be 0
+      And the output should contain:
+        """
+         * adding remote origin
+        remote origin added!
+        """
       And the vimpack git remote "origin" should be "git@github.com:bramswenson/vimpack-repo.git"
+
--- lib/vimpack/commands/git.rb
+++ lib/vimpack/commands/git.rb
@@ -1,9 +1,27 @@
 module Vimpack
   module Commands
     class Git < Command
+      SUB_COMMANDS = %w{ remote }
+
+      def initialize_commands
+        die!("git requires at least one argument") if @commands.size < 1
+        @subcommand = @commands.shift
+      end
 
       def run
-        cmd = "git #{@commands.join(' ')}"
+        git_exec unless SUB_COMMANDS.include?(@subcommand)
+        send("git_#{@subcommand}".to_sym)
+      end
+
+      def git_remote
+        cmd = "git remote add origin #{@commands.join(' ')}"
+        say(' * adding remote origin')
+        run_process_or_die!(cmd, self.pack_path.to_s)
+        say('remote origin added!')
+      end
+      
+      def git_exec
+        cmd = "git #{@subcommand} #{@commands.join(' ')}"
         say(" * running #{cmd}")
         run_process_or_die!(cmd, self.pack_path.to_s)
       end
